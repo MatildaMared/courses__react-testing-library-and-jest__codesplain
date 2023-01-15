64f3349b7f4e249d599ecae9f8e101a1361b67ac
Matilda Mared <matildamared@live.se>
2023-01-15 09:50:40 +0100

    add tests for authbuttons component

---
+++ src/components/auth/AuthButtons.test.js
@@ -0,0 +1,87 @@
+import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router";
+import { createServer } from "../../test/server";
+import AuthButtons from "./AuthButtons";
+
+async function renderComponent() {
+	render(
+		<MemoryRouter>
+			<AuthButtons />
+		</MemoryRouter>
+	);
+
+	await screen.findAllByRole("link");
+}
+
+describe("AuthButtons component", () => {
+	describe("when the user is not signed in", () => {
+		createServer([
+			{
+				url: "/api/user",
+				res: (req, res, ctx) => {
+					return { user: null };
+				},
+			},
+		]);
+
+		it("displays a sign in and sign up button", async () => {
+			await renderComponent();
+
+			const signInButton = screen.getByRole("link", {
+				name: /sign in/i,
+			});
+			const signUpButton = screen.getByRole("link", {
+				name: /sign up/i,
+			});
+
+			expect(signInButton).toBeInTheDocument();
+			expect(signUpButton).toBeInTheDocument();
+		});
+
+		it("does not display a sign out button", async () => {
+			await renderComponent();
+
+			const signOutButton = screen.queryByRole("link", {
+				name: /sign out/i,
+			});
+
+			expect(signOutButton).not.toBeInTheDocument();
+		});
+	});
+
+	describe("when the user is signed in", () => {
+		createServer([
+			{
+				url: "/api/user",
+				res: (req, res, ctx) => {
+					return { user: { id: 3, email: "test@test.com" } };
+				},
+			},
+		]);
+
+		it("does not display a sign in and sign up button", async () => {
+			await renderComponent();
+
+			const signInButton = screen.queryByRole("link", {
+				name: /sign in/i,
+			});
+
+			const signUpButton = screen.queryByRole("link", {
+				name: /sign up/i,
+			});
+
+			expect(signInButton).not.toBeInTheDocument();
+			expect(signUpButton).not.toBeInTheDocument();
+		});
+
+		it("displays a sign out button", async () => {
+			await renderComponent();
+
+			const signOutButton = screen.getByRole("link", {
+				name: /sign out/i,
+			});
+
+			expect(signOutButton).toBeInTheDocument();
+		});
+	});
+});
